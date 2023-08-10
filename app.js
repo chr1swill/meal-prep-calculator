d6af9a8f1ccb73fda2be691527f0d0275bd09655
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-10 19:10:21 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// ask for total grams of chicken and days
+// multiply total grams of chicken by 0.23 to get total grams of protein
+// display total grams of protein on screen
+// divide total grams of chicken by days to get grams of chicken per day
+// display total grams og chicken per a day on screen
+// divide total grams of protein by days to get grams of protein per day
+// display total grams protein per a day on screen
+
+document.getElementById("myForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const totalGramsOfChicken = parseFloat(
+    document.getElementById("total-grams-of-chicken").value
+  );
+  const totalDays = parseFloat(document.getElementById("total-days").value);
+
+  if (isNaN(totalGramsOfChicken) || isNaN(totalDays)) {
+    alert("Please enter a valid number");
+    return;
+  }
+
+  const gramsOfChickenPerDay = calculateGramsOfChickenPerDay(
+    totalGramsOfChicken,
+    totalDays
+  ).toFixed(2);
+  const proteinPerDay = calculateProteinPerDay(
+    totalGramsOfChicken,
+    totalDays
+  ).toFixed(2);
+  const totalProtein = calculateTotalProtein(totalGramsOfChicken).toFixed(2);
+
+  document.getElementById(
+    "total-protein-result"
+  ).innerHTML = `${totalProtein}g of protein total`;
+  document.getElementById(
+    "chicken-per-day-result"
+  ).innerHTML = `${gramsOfChickenPerDay}g of chicken per day`;
+  document.getElementById(
+    "protein-per-day-result"
+  ).innerHTML = `${proteinPerDay}g of protein per day`;
+});
+
+const PROTEIN_PER_GRAM_OF_CHICKEN = 0.23;
+
+function calculateGramsOfChickenPerDay(totalGramsOfChicken, totalDays) {
+  return totalGramsOfChicken / totalDays;
+}
+
+function calculateProteinPerDay(totalGramsOfChicken, totalDays) {
+  const gramsOfChickenPerDay = calculateGramsOfChickenPerDay(
+    totalGramsOfChicken,
+    totalDays
+  );
+  return calculateTotalProtein(gramsOfChickenPerDay);
+}
+
+function calculateTotalProtein(gramsOfChicken) {
+  return gramsOfChicken * PROTEIN_PER_GRAM_OF_CHICKEN;
+}
