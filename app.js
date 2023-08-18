d4e69f874c162f134a9bd9a65adf63b07df92739
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-08-17 23:11:10 -0400

    refactor - move global var to top with logic

--- app.js
+++ app.js
@@ -7,44 +7,13 @@
 // display total grams protein per a day on screen
 
 const PROTEIN_PER_GRAM_OF_CHICKEN = 0.23;
-const totalGramsOfChicken = parseFloat(
-  document.getElementById("total-grams-of-chicken").value
-);
-const totalDays = parseFloat(document.getElementById("total-days").value);
-
-document.getElementById("myForm").addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  if (isNaN(totalGramsOfChicken) || isNaN(totalDays)) {
-    alert("Please enter a valid number");
-    return;
-  }
-
-  const gramsOfChickenPerDay = calculateGramsOfChickenPerDay(
-    totalGramsOfChicken,
-    totalDays
-  ).toFixed(2);
-  const proteinPerDay = calculateProteinPerDay(
-    totalGramsOfChicken,
-    totalDays
-  ).toFixed(2);
-  const totalProtein = calculateTotalProtein(totalGramsOfChicken).toFixed(2);
-
-  document.getElementById(
-    "total-protein-result"
-  ).innerHTML = `${totalProtein}g of protein total`;
-  document.getElementById(
-    "chicken-per-day-result"
-  ).innerHTML = `${gramsOfChickenPerDay}g of chicken per day`;
-  document.getElementById(
-    "protein-per-day-result"
-  ).innerHTML = `${proteinPerDay}g of protein per day`;
-});
 
 const calculateGramsOfChickenPerDay = (totalGramsOfChicken, totalDays) => {
   return totalGramsOfChicken / totalDays;
 };
-
+const calculateTotalProtein = (gramsOfChicken) => {
+  return gramsOfChicken * PROTEIN_PER_GRAM_OF_CHICKEN;
+};
 const calculateProteinPerDay = (totalGramsOfChicken, totalDays) => {
   const gramsOfChickenPerDay = calculateGramsOfChickenPerDay(
     totalGramsOfChicken,
@@ -53,6 +22,37 @@ const calculateProteinPerDay = (totalGramsOfChicken, totalDays) => {
   return calculateTotalProtein(gramsOfChickenPerDay);
 };
 
-const calculateTotalProtein = (gramsOfChicken) => {
-  return gramsOfChicken * PROTEIN_PER_GRAM_OF_CHICKEN;
-};
+document.getElementById("myForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const totalGramsOfChicken = parseFloat(
+    document.getElementById("total-grams-of-chicken").value
+  );
+  const totalDays = parseFloat(document.getElementById("total-days").value);
+
+  if (isNaN(totalGramsOfChicken) || isNaN(totalDays)) {
+    alert("Please enter a valid number");
+    return;
+  }
+
+  const gramsOfChickenPerDayAsResult = calculateGramsOfChickenPerDay(
+    totalGramsOfChicken,
+    totalDays
+  ).toFixed(2);
+  const proteinPerDayAsResult = calculateProteinPerDay(
+    totalGramsOfChicken,
+    totalDays
+  ).toFixed(2);
+  const totalProteinAsResult =
+    calculateTotalProtein(totalGramsOfChicken).toFixed(2);
+
+  document.getElementById(
+    "total-protein-result"
+  ).innerHTML = `${totalProteinAsResult}g of protein total`;
+  document.getElementById(
+    "chicken-per-day-result"
+  ).innerHTML = `${gramsOfChickenPerDayAsResult}g of chicken per day`;
+  document.getElementById(
+    "protein-per-day-result"
+  ).innerHTML = `${proteinPerDayAsResult}g of protein per day`;
+});
