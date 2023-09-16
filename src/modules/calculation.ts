87acfd504a7c870a52c339b1175c5c9a7df368d0
chr1swill <127428764+chr1swill@users.noreply.github.com>
2023-09-16 13:52:07 -0400

    fix - add types to calculation.ts

--- src/modules/calculation.ts
+++ src/modules/calculation.ts
@@ -1,20 +1,30 @@
-export const PROTEIN_PER_GRAM_OF_CHICKEN = 0.23;
+export const PROTEIN_PER_GRAM_OF_CHICKEN: number = 0.23;
 
-export const calculateGramsOfChickenPerDay = (totalGramsOfChicken, totalDays) => {
-    return totalGramsOfChicken / totalDays;
+export const calculateGramsOfChickenPerDay = (
+  totalGramsOfChicken: number,
+  totalDays: number
+): number => {
+  return totalGramsOfChicken / totalDays;
 };
-export const calculateTotalProtein = (gramsOfChicken) => {
-    return gramsOfChicken * PROTEIN_PER_GRAM_OF_CHICKEN;
+export const calculateTotalProtein = (gramsOfChicken: number): number => {
+  return gramsOfChicken * PROTEIN_PER_GRAM_OF_CHICKEN;
 };
-export const calculateProteinPerDay = (totalGramsOfChicken, totalDays) => {
-    const gramsOfChickenPerDay = calculateGramsOfChickenPerDay(
-        totalGramsOfChicken,
-        totalDays
-    );
-    return calculateTotalProtein(gramsOfChickenPerDay);
+export const calculateProteinPerDay = (
+  totalGramsOfChicken: number,
+  totalDays: number
+): number => {
+  const gramsOfChickenPerDay = calculateGramsOfChickenPerDay(
+    totalGramsOfChicken,
+    totalDays
+  );
+  return calculateTotalProtein(gramsOfChickenPerDay);
 };
 
-export const getSplitString = (stringList) => stringList.split(/[,\s]+/);
-export const getStringToArray = (stringList) =>
-    getSplitString(stringList).map(parseFloat);
-export const getArrayTotal = (array) => array.reduce((a, b) => a + b);
+export const getSplitString = (stringList: string): string[] =>
+  stringList.split(/[,\s]+/);
+export const getStringToArray = (stringList: string): number[] => {
+  return getSplitString(stringList).map(parseFloat);
+};
+export const getArrayTotal = (array: number[]): number => {
+  return array.reduce((a, b) => a + b);
+};
